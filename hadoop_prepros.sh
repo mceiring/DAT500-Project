6a96ac1fa4a1b5109fea472c8c015cb702fa782e
mceiring <erlendeiring@gmail.com>
2023-04-24 11:00:52 +0000

    yuh

--- hadoop_prepros.sh
+++ hadoop_prepros.sh
@@ -18,17 +18,8 @@ hadoop fs -rm -r /chess_2016_dataset/output
 python3 test/test_mapreduce.py \
      --hadoop-streaming-jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.4.jar \
      --jobconf mapred.output.textoutputformat.separator=" " \
-     -r hadoop hdfs://namenode:9000/chess_2016_dataset/input_files/*.txt \
+     -r hadoop hdfs://namenode:9000/chess_2016_dataset/input_files/chess_part_1.txt \
      --output-dir hdfs://namenode:9000/chess_2016_dataset/output --no-output
-     
-
-### TEST RUN WITH LIMITED DATA
-# # runs current files, with specified input and output
-# python3 test/test_mapreduce.py \
-#      --hadoop-streaming-jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.4.jar \
-#      --jobconf mapred.textoutputformat.separator=" " \
-#      -r hadoop hdfs://namenode:9000/chess_2016_dataset/chess_part_1.txt \
-#      --output-dir hdfs://namenode:9000/chess_2016_dataset/output --no-output
 
 # transfere output to csv file
 # hadoop fs -text /chess_2016_dataset/output/part* > chess_2016_dataset/chess_data_1.csv
